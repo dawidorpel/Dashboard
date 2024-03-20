9a8be58e3d0a911514362759b640ce0c2a343be0
dawidorpel <143597333+dawidorpel@users.noreply.github.com>
2024-03-20 16:43:26 +0100

    js script events

---
+++ script.js
@@ -0,0 +1,34 @@
+const sideLinks = document.querySelectorAll('.Sidebar .side-menu li a:not(.logout)');
+
+sideLinks.forEach(item => {
+    const li = item.parentElement;
+    item.addEventListener('click' , () =>{
+        sideLinks.forEach(i =>{
+            i.parentElement.classList.remove('active');
+        })
+        li.classList.add('active');
+    })
+    });
+
+    const menuBar = document.querySelector('.content nav .bx.bx-menu');
+    const sideBar = document.querySelector('.Sidebar');
+    
+    menuBar.addEventListener('click', () => {
+        sideBar.classList.toggle('close');
+    });
+
+   const searchBtn = document.querySelector('.content nav form .form-input button');
+   const searchBtnIcon = document.querySelector('.content nav form .form-input button .bx');
+   const searchForm = document.querySelector('.content nav form');
+
+   searchBtn.addEventListener('click', function (e) {
+    if(window.innerWidth < 576){
+        e.preventDefault;
+        searchForm.classList.toggle('show');
+        if(searchForm.classList.contains('show')){
+            searchBtnIcon.classList.replace('bx-search', 'bx-x');
+        }else{
+            searchBtnIcon.classList.replace('bx-x', 'bx-search')
+        }
+    }
+   })
